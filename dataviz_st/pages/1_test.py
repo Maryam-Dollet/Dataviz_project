13bb446fdb418440f6d1420e29a54ae60ac39f2a
Maryam-Dollet <maryam.dollet@gmail.com>
2023-12-30 19:34:34 +0100

    change streamlit

--- dataviz_st/pages/1_test.py
+++ dataviz_st/pages/1_test.py
@@ -2,20 +2,28 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+
 @st.cache_data
 def load_datasets():
-    df_athlete = pd.read_csv("athlete_events.csv", sep=',')
-    noc_regions = pd.read_csv("noc_regions.csv", sep=',')
-    df_merged = df_athlete.merge(noc_regions, how='left', on="NOC")
+    df_athlete = pd.read_csv("athlete_events.csv", sep=",")
+    noc_regions = pd.read_csv("noc_regions.csv", sep=",")
+    df_merged = df_athlete.merge(noc_regions, how="left", on="NOC")
     return df_merged
+
 
 df_merged = load_datasets()
 
 st.dataframe(df_merged)
 
 st.subheader("Participans Gender Total")
-df1 = df_merged.value_counts("Sex").reset_index().rename(columns={"count":"nb_sex"})
+df1 = (
+    df_merged.value_counts("Sex")
+    .reset_index()
+    .rename(columns={"Sex": "Gender", "count": "gender_count"})
+)
 st.dataframe(df1)
 
-fig = px.bar(df1, x="Sex", y="nb_sex")
+fig = px.bar(df1, x="Gender", y="gender_count")
 st.plotly_chart(fig)
+
+st.subheader("Gender Repartition Through the Years")
