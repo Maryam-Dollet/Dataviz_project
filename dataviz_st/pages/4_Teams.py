b8779c70b506b393d283eccc3da047bd0813162f
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-03 16:10:54 +0100

    typo correction

--- dataviz_st/pages/4_Teams.py
+++ dataviz_st/pages/4_Teams.py
@@ -9,7 +9,6 @@ st.header("Team Analysis")
 season = option_menu("Choose Season", ["Summer", "Winter"], orientation="horizontal")
 df_regions = get_regions(season)
 df_city_positions = get_city_position(season)
-st.dataframe(df_city_positions)
 
 fig = px.choropleth(
     df_regions,
@@ -17,7 +16,7 @@ fig = px.choropleth(
     color="participants number",
     hover_name="region",
     animation_frame="Year",
-    title=f"Participating Countries of the {season} Games Each Years",
+    title=f"Participating Countries of the {season} Games Each Year",
     color_continuous_scale="Viridis",
     locationmode="country names",
     width=1200,
@@ -26,21 +25,11 @@ fig = px.choropleth(
 
 st.plotly_chart(fig)
 
-
-# fig = go.Figure(
-#     data=go.Scattergeo(
-#         lon=df_city_positions["longitude"],
-#         lat=df_city_positions["latitude"],
-#         text=df_city_positions["description"],
-#         mode="markers",
-#     )
-# )
-
 fig = px.scatter_geo(
     df_city_positions,
     lat=df_city_positions.latitude,
     lon=df_city_positions.longitude,
     hover_name="description",
 )
-fig.update_layout(width=1000, height=800)
+fig.update_layout(width=1000, height=800, title=f"Host Cities of {season} Games")
 st.plotly_chart(fig)
