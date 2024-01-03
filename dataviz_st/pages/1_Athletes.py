b02b77829ac188239ea4eaa38a45d5b73f764af9
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-03 01:05:20 +0100

    add pages

--- dataviz_st/pages/1_Athletes.py
+++ dataviz_st/pages/1_Athletes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from dataviz_st.cache_func import load_datasets
+from cache_func import load_datasets
 
 
 df_merged = load_datasets()
