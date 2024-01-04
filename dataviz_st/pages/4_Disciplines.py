14cbc1f0e1cb06f12e48b7a2a9af3a9ae1ec7010
Maryam-Dollet <maryam.dollet@gmail.com>
2024-01-04 17:26:03 +0100

    update discipline page

--- dataviz_st/pages/4_Disciplines.py
+++ dataviz_st/pages/4_Disciplines.py
@@ -1,16 +1,46 @@
 import streamlit as st
 import plotly.express as px
-from cache_func import get_city_position
+from cache_func import load_datasets
 from streamlit_option_menu import option_menu
 
-st.header("Disciplines")
-# season = option_menu("Choose Season", ["Summer", "Winter"], orientation="horizontal")
-# df_city_positions = get_city_position(season)
-# st.dataframe(df_city_positions)
+st.set_page_config(layout="wide")
 
-# fig = px.scatter_geo(df_city_positions,
-#                     lat=df_city_positions.latitude,
-#                     lon=df_city_positions.longitude,
-#                     hover_name="description")
-# fig.update_layout(width=1000, height=800)
-# st.plotly_chart(fig)
+with st.sidebar:
+    season = option_menu("Choose Season", ["Summer", "Winter"])
+
+st.header("Disciplines")
+st.subheader("Distributed Medals")
+
+df_merged = load_datasets()
+games = df_merged[df_merged.Season == season]
+
+medals = (
+    games.groupby(["Year", "Sport", "Medal"]).size().reset_index(name="Nb of medals")
+)
+# st.dataframe(medals.head(3))
+
+medals = medals.pivot_table(
+    index=["Year", "Sport"], columns="Medal", values="Nb of medals", aggfunc="sum"
+).reset_index()
+# st.dataframe(medals.head(15))
+
+medals = medals.fillna(0)
+medals = medals.astype({"Gold": "int32", "Silver": "int32", "Bronze": "int32"})
+
+medals["Total Nb of Medals distributed"] = (
+    medals["Gold"] + medals["Silver"] + medals["Bronze"]
+)
+# st.dataframe(medals.head(15))
+
+fig = px.bar(
+    medals,
+    x="Sport",
+    y=["Gold", "Silver", "Bronze"],
+    title=f"Number of Medals distributed per Sport in {season} Games",
+    width=1450,
+    height=800,
+)
+fig.update_layout(barmode="stack", xaxis={"categoryorder": "total descending"})
+fig.update_xaxes(tickangle=45)
+
+st.plotly_chart(fig)
